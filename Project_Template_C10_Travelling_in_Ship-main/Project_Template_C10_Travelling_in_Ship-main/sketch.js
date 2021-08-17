9b0485620ad38ea73691e421190bb4dcb4ddc570
navaneethlibi <88582268+navaneethlibi@users.noreply.github.com>
2021-08-17 16:59:14 +0530

    Update sketch.js

--- Project_Template_C10_Travelling_in_Ship-main/Project_Template_C10_Travelling_in_Ship-main/sketch.js
+++ Project_Template_C10_Travelling_in_Ship-main/Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -8,13 +8,14 @@ groundimage=loadImage("sea.png");
 
 function setup(){
   createCanvas(400,400);
-  ship = createSprite(250,150,20,50);
-  ship.addAnimation("moving",ship_moving);
+ 
   edges =createEdgeSprites() ;
-ground = createSprite(300,160,400,10);
+ground = createSprite(300,180,400,10);
 ground.addImage(groundimage);
-ground.velocityX=0
-ship.scale = 0.5;
+ground.velocityX=-3
+ship = createSprite(300,250,20,50);
+ship.addAnimation("moving",ship_moving);
+ship.scale = 0.3;
 ship.x = 50
 }
 
@@ -25,15 +26,12 @@ function draw() {
   console.log(ship)
   
   //jump when space key is pressed
-  if(keyDown("space")){
-   ship .velocityY = 10;  }
-  ship.velocityY =ship .velocityY + 0.5;
+  
   if (ground.x<0){
 ground.x=ground.width/2;
   }
 
-  ship.collide(ground)
+  
   drawSprites();
-
 
   }
